84b26357cc05909fe473b2ae02a633ff482ed23c
Jetfried <app.iwhite@outlook.com>
2023-12-15 15:35:40 -0500

    Update RobotMecanumDrive.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/RobotMecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/RobotMecanumDrive.java
@@ -55,8 +55,8 @@ import static org.firstinspires.ftc.teamcode.drive.DriveConstants.kV;
  */
 @Config
 public class RobotMecanumDrive extends MecanumDrive {
-    public static PIDCoefficients TRANSLATIONAL_PID = new PIDCoefficients(0.1, 0, 0);
-    public static PIDCoefficients HEADING_PID = new PIDCoefficients(0.8, 0, 0);
+    public static PIDCoefficients TRANSLATIONAL_PID = new PIDCoefficients(0.09, 0, 0);
+    public static PIDCoefficients HEADING_PID = new PIDCoefficients(0.7, 0, 0);
 
     public static double LATERAL_MULTIPLIER = 1;
 
